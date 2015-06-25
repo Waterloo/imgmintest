d3df0074229c2492fd1051949cae7c36759d3b54
riddler <cvdrops@outlook.com>
2015-06-25 12:56:35 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require("express"),
     Imagemin = require('imagemin'),
     serveIndex = require('serve-index');
 
-
+var glob='';
 app.use(express.static(__dirname + '/public'));
 app.use('/public', serveIndex(__dirname + '/public'));
 
@@ -13,10 +13,11 @@ new Imagemin()
     .use(Imagemin.jpegtran({progressive: true}))
     .run(function (err, files) {
         console.log(files[0]);
+    glob ={"err":err, "fil":files[0]};
         // => {path: 'build/images/foo.jpg', contents: <Buffer 89 50 4e ...>}
     });
     
-
+app.get('/',function (req,res) {res.json(glob)});
     
     
     
